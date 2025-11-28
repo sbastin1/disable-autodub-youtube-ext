7d2ee417d38706c05facfa4354be33e3cb1e00e7
Sebastian Wilden <sebastian.wilden@outlook.com>
2025-11-28 18:40:26 +0100

    init

---
+++ src/contentScripts/index.ts
@@ -0,0 +1,54 @@
+function onNavigate() {
+  const settingsButton: HTMLElement | null = document?.querySelector(".ytp-settings-button")
+
+  if (settingsButton) {
+    settingsButton.click()
+
+    const audioMenuItem: HTMLElement | null = document.querySelector(".ytp-audio-menu-item")
+
+    if (!audioMenuItem) {
+      settingsButton.click()
+      return;
+    }
+
+    const audioMenuContent = audioMenuItem.querySelector(".ytp-menuitem-content")
+    const isAlreadyOriginal = audioMenuContent?.textContent?.includes("original")
+
+    if (isAlreadyOriginal) {
+      settingsButton.click()
+      return;
+    }
+
+    audioMenuItem.click()
+
+    const menuItems= document.querySelectorAll<HTMLElement>(".ytp-menuitem")
+    menuItems.forEach((item) => {
+      const label = item?.firstChild?.textContent
+      if (label?.includes("original")) {
+        item.click()
+
+        // Race condition: Youtube needs time to switch tracks
+        // TODO: Check if youtube fires any event on audio track switch
+        setTimeout(() => {
+          settingsButton.click() 
+        }, 1000)
+      }
+    })
+  }
+}
+
+window.addEventListener("yt-navigate-finish", onNavigate);
+
+(function() {
+  const pushState = history.pushState;
+  history.pushState = function(...args) {
+    pushState.apply(this, args);
+    onNavigate();
+  };
+  const replaceState = history.replaceState;
+  history.replaceState = function(...args) {
+    replaceState.apply(this, args);
+    onNavigate();
+  };
+  window.addEventListener("popstate", onNavigate);
+})();
---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from "vite";
+import webExtension, { readJsonFile } from "vite-plugin-web-extension";
+
+function generateManifest() {
+  const manifest = readJsonFile("src/manifest.json");
+  const pkg = readJsonFile("package.json");
+  return {
+    name: pkg.name,
+    description: pkg.description,
+    version: pkg.version,
+    ...manifest,
+  };
+}
+
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    webExtension({
+      manifest: generateManifest,
+      browser: mode === "firefox" ? "firefox" : "chrome",
+      watchFilePaths: ["package.json", "manifest.json", "./src/contentScripts/index.ts"],
+      webExtConfig: {
+        target: "chromium",
+        chromiumBinary: "/usr/sbin/brave",
+      }
+    }),
+  ],
+}));
